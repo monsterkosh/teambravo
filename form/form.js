4646d232a9c8403672cb8b16907472572eea2957
analia mancuso <analiamancuso@live.com>
2021-10-22 22:17:45 -0300

    form

---
+++ form/form.js
@@ -0,0 +1,16 @@
+
+(function() {
+    emailjs.init("user_aUQaLaKkjPI96V8O45yPq");
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        emailjs.sendForm("service_6gq48gb","template_fu895c4", this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+      
+    });
+}
