75e67df3483f691f0fec88ba762c802bcb9e9f22
monsterkosh <mnnnkosh@gmail.com>
2021-10-26 10:52:51 -0300

    final

--- form/form.js
+++ form/form.js
@@ -1,20 +1,17 @@
 // @ts-nocheck
-(function () {
-  emailjs.init('user_aUQaLaKkjPI96V8O45yPq');
-})();
-
 function send() {
   document
     .getElementById('contact-form')
-    .addEventListener('submit', function (event) {
-      event.preventDefault();
-      emailjs.sendForm('service_6gq48gb', 'template_fu895c4', this).then(
-        function () {
-          alert('Sent!!');
-        },
-        function (error) {
-          Rollbar.warning('FAILED to send message');
-        }
-      );
+    .addEventListener('submit', function (e) {
+      e.preventDefault(),
+        emailjs.sendForm('service_6gq48gb', 'template_fu895c4', this).then(
+          function () {
+            alert('Sent!!');
+          },
+          function (e) {
+            Rollbar.warning('FAILED to send message');
+          }
+        );
     });
 }
+emailjs.init('user_aUQaLaKkjPI96V8O45yPq');
